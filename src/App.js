3265c000c46f5aa990d89593a02c7296b2549630
Nevyana Angelova <nevyangelova@Nevyanas-MacBook-Pro.local>
2022-05-24 21:28:20 +0300

    nitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import ObjectWrapper from './components/Object';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <ObjectWrapper />
+        </div>
+    );
 }
 
 export default App;
